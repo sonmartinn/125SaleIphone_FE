2d2b2473380ca0ac9f3d95be03f860ae88294801
NgVanBao <baox2pro@gmail.com>
2026-01-13 00:36:35 +0700

    up_lib

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 
 async function apiFetch(
   endpoint: string,
-  options: RequestInit & { isFormData?: boolean } = {}
+  options: Omit<RequestInit, 'body'> & { body?: any; isFormData?: boolean } = {}
 ) {
   const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
 
